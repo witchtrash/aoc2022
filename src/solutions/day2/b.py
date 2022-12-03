d5fbe1d4a6d818b4cde40e21b0d0daab00474272
emilia <emilia@hakke.ro>
2022-12-03 11:09:11 +0000

    Scaffold days 1 and 2

---
+++ src/solutions/day2/b.py
@@ -0,0 +1,16 @@
+from src.lib.util import Input, get_problem_input
+
+problem_input: Input = get_problem_input()
+test_input: Input = get_problem_input(test=True)
+
+
+def solve(problem_input: Input) -> str:
+    return problem_input.raw()
+
+
+def test() -> str:
+    return str(solve(test_input))
+
+
+def run() -> str:
+    return str(solve(problem_input))
